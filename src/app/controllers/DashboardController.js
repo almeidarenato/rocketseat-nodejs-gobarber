a255b3430aa6a111220ee14a3d20b0d0e13b52fb
almeidarenato <renatoam@ymail.com>
2019-05-05 12:16:42 -0300

    visualizaçao de agendamentos para cabeleireiros

--- src/app/controllers/DashboardController.js
+++ src/app/controllers/DashboardController.js
@@ -1,7 +1,26 @@
-const { User } = require('../models')
-
+const { User, Appointment } = require('../models')
 class DashboardController {
   async index (request, response) {
+    // console.log(user)
+    const { provider } = request.session.user
+    const { id } = request.session.user
+    console.log(id)
+    console.log(provider)
+    if (provider) {
+      const appointments = await Appointment.findAll({
+        where: {
+          provider_id: id
+        },
+        include: {
+          model: User,
+          as: 'User'
+        }
+      })
+
+      console.log(appointments[0].User.name)
+      console.log(appointments[1].User.name)
+      return response.render('dashboard_provider', { appointments })
+    }
     const providers = await User.findAll({ where: { provider: true } })
     return response.render('dashboard', { providers })
   }
